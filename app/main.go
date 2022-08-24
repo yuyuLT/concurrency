50c7eb5822a10d0897d6cbc3a77f08ec0f7cb173
yuyuLT <wawawawadaiko@icloud.com>
2022-08-24 17:38:17 +0900

    use waitGroup

--- app/main.go
+++ app/main.go
@@ -3,15 +3,32 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
 func main() {
 	fmt.Println("Hello, concurrency!")
-	go delay()
 
-	time.Sleep(time.Second * 1)
+	var wg sync.WaitGroup
+	
+	wg.Add(2)
+
+	go printMessage(&wg)
+	go printMessage2(&wg)
+
+	wg.Wait()
+
+	fmt.Println("finish program!")
 }
 
-func delay() {
-	fmt.Println("Hello, goroutine!")
+func printMessage(wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(time.Second * 5)
+	fmt.Println("this is message 1")
+}
+
+func printMessage2(wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(time.Second * 5)
+	fmt.Println("this is message 2")
 }
